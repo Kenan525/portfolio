1e9bfb6c7f4ba8cfdcbb52f3c17b906002f6cb56
kenan mahmic <kenan_mahmic@hotmail.com>
2022-12-17 22:41:51 +0100

    feat:contact form changes

--- src/app/loading/loading.component.ts
+++ src/app/loading/loading.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-loading',
@@ -18,6 +18,9 @@ export class LoadingComponent implements OnInit {
 
   ngOnInit(): void {
 
+  }
+
+  ngAfterViewInit(): void {
   }
 
 }
